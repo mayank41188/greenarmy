7ad26938ee6e25137c9f25c3ee1f7999b00bc09c
Mayank Sharma <mayank@simbiotiktech.com>
2022-05-30 15:45:55 +0530

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomeModule } from './home/home.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './home/landing-page/landing-page.component';
+import { SharedModule } from './shared/shared.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HomeModule,
+    AppRoutingModule,
+    SharedModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
