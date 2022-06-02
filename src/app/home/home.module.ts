f4cf9384a623a92824f1caa7714baf9f01b62cf5
Mayank Sharma <mayank@simbiotiktech.com>
2022-06-03 01:08:45 +0530

    fix

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SharedModule } from '../shared/shared.module';
 
 
@@ -8,7 +9,7 @@ import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-
+   
   ],
   imports: [
     CommonModule,
