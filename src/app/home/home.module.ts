6d32e232807d09eafed80e6277a6f61a9d34609d
ravipaghdar <ravi.p@simbiotiktech.com>
2022-06-03 16:16:09 +0530

    Create New Component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,16 +3,26 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeLoginComponent } from './employee-login/employee-login.component';
+import { EmployeeLoginOtpComponent } from './employee-login-otp/employee-login-otp.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent,
   },
+  {
+    path: 'employeeLogin',
+    component: EmployeeLoginComponent,
+  },
+  {
+    path: 'employeeLoginOtp',
+    component: EmployeeLoginOtpComponent,
+  },
 ];
 
 @NgModule({
-  declarations: [LandingPageComponent],
+  declarations: [LandingPageComponent, EmployeeLoginComponent, EmployeeLoginOtpComponent],
   imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
 })
 export class HomeModule {}
