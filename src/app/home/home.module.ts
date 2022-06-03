ff59c0775c0d8405cfcbf0a82b1e7686356415ad
Mayank Sharma <mayank@simbiotiktech.com>
2022-06-03 16:07:53 +0530

    Employee-Profile-Page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,16 +3,21 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeProfileComponent } from './employee-profile/employee-profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent,
   },
+  {
+    path: 'employee',
+    component: EmployeeProfileComponent,
+  },
 ];
 
 @NgModule({
-  declarations: [LandingPageComponent],
+  declarations: [LandingPageComponent, EmployeeProfileComponent],
   imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
 })
 export class HomeModule {}
